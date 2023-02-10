19d3b7fad0cbfa3bad48b8f96af7cabb99fec344
Kari Adilova <kariadilovaadilova7@gmail.com>
2023-02-10 23:15:54 +0500

    feat: implement Reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+    const nStr = n.toString();
+    let result = '';
+    for (let i = 0; i < nStr.length; i++) {
+        result  = nStr[i] + result;
+    }
+    return Number(result); 
 }
